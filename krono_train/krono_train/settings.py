cdb9ae4264b6a1c2ba017c0296343218aec60644
Ricaldo <ricardo.playful@gmail.com>
2018-02-15 17:17:25 -0400

    V 1.0

--- krono_train/krono_train/settings.py
+++ krono_train/krono_train/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z1r8llp1y-z$+pz!pc!&igeny2$o=u7y6f^k!qp*l+@+03nz*u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1', 'krono-train-dev.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
