9afc2330521e22549f1bf7960cb04a2557cd7c2b
Ricaldo <ricardo.playful@gmail.com>
2018-02-15 17:07:33 -0400

    V 1.0

--- krono_train/krono_train/wsgi.py
+++ krono_train/krono_train/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "krono_train")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "krono_train.settings")
 
+#application = get_wsgi_application()
+
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'krono_train.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "krono_train.settings")
 application = get_wsgi_application()
